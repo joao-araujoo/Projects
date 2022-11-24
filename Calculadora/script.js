84bbe40a941512652fb73a40bd06673531d6076e
joao-araujoo <joao.araujoo2007@gmail.com>
2022-11-24 11:47:01 -0300

    script

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,3 +1,21 @@
 function insert(num){
     document.querySelector('div.result').innerHTML += num
 }
+
+function clean(){
+    document.querySelector('div.result').innerHTML = ''
+}
+
+function backspace(){
+    let resultado = document.querySelector('div.result').innerHTML
+    document.querySelector('div.result').innerHTML = resultado.substring(0, resultado.length -1)
+}
+
+function calculate(){
+    let resultado = document.querySelector('div.result').innerHTML
+    if(resultado){
+        document.querySelector('div.result').innerHTML = eval(resultado)
+    } else {
+        alert('Type something to calculate!')
+    }
+}
