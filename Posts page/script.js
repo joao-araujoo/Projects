2826e24952b32f76bbf0982ce0ad2b429dbe58c7
joao-araujoo <joao.araujoo2007@gmail.com>
2022-12-27 18:11:37 -0300

    easter egg

--- Posts page/script.js
+++ Posts page/script.js
@@ -1,6 +1,7 @@
 let profilePicture = 'https://www.promoview.com.br/uploads/2017/04/b72a1cfe.png'
 let profileName = 'Anônimo'
 let postOrder = 1
+let easterEggCount = 0
 
 function createPost(){
     const postsSection = document.querySelector('section.posts')
@@ -86,5 +87,33 @@ function changeUserPhoto(){
         document.getElementById('user-picture').width = 50
     } else {
         alert('Insira algum link!')
+    }
+}
+
+function easterEggButton(){
+    easterEggCount++
+    const easterEggContent = document.getElementById('easterEggContent')
+    switch(easterEggCount){
+        case 5:
+            easterEggContent.innerText = "I told you, it's impossible to erase me, rs"
+            break
+        case 10:
+            easterEggContent.innerText = "bro, why are you so insistent??? stop trying"
+            break
+        case 15:
+            easterEggContent.innerText = "LOL 🤣😂🤣😂😂🤣😂🤣🤣😂🤣😂😂🤣🤣🤣😂🤣😂😂🤣🤣🤣😂🤣😂🤣😂🤣😂🤣😂😂"
+            break
+        case 20:
+            easterEggContent.innerText = "I ALREADY TOLD YOU, YOU CANT REMOVE ME MF"
+            break
+        case 25:
+            easterEggContent.innerText = "ok... you won...💔💔"
+            break
+        case 30:
+            easterEggContent.innerText = "JUST KIDDING LOL NO CLUE YOU BELIEVED"
+            break
+    }
+    if(easterEggCount > 34){
+        easterEggContent.innerText = "now i'm serious, stop before your mom comes and curses you because you haven't showered yet, you greasy"
     }
 }
