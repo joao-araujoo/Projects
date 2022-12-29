388cad3f90dae12a66d749399866d1e09868fc5a
joao-araujoo <joao.araujoo2007@gmail.com>
2022-12-29 17:13:06 -0300

    modal script

--- Posts page/javascript/modal.js
+++ Posts page/javascript/modal.js
@@ -3,18 +3,24 @@ function openModal(){
 
     const inputTitle = document.createElement('p')
     inputTitle.innerText = 'Digite aqui:'
+    inputTitle.id = 'modal-input-title'
 
     const contentInput = document.createElement('textarea')
     contentInput.maxLength = 500
+    contentInput.id = 'modal-content-input'
 
-    const sendButton = document.createElement('button')
-    sendButton.innerText = 'Post'
+    const postButton = document.createElement('button')
+    postButton.innerText = 'Post'
+    postButton.id = 'modal-post-button'
 
     const modal = document.querySelector('div.modal')
-    modal.append(inputTitle, contentInput, sendButton)
+    modal.append(inputTitle, contentInput, postButton)
 
 }
 
 function closeModal(){
     document.getElementById('modal').style.top = '-100%'
+    document.getElementById('modal-input-title').remove()
+    document.getElementById('modal-content-input').remove()
+    document.getElementById('modal-post-button').remove()
 }
