3a0cfede7d327a83c5696b47da4e4d84c7cf2fc1
joao-araujoo <joao.araujoo2007@gmail.com>
2022-12-30 13:58:16 -0300

    copy button animation

--- Posts page/javascript/easterEgg.js
+++ Posts page/javascript/easterEgg.js
@@ -44,7 +44,9 @@ document.getElementById('post').addEventListener('dblclick', function(){
 
 function copyText(){
     const content = document.querySelector('div#easterEggContent')
-    navigator.clipboard.writeText(content.innerText).then(() => {
-        alert('copied to clipboard')
-    })
+    setTimeout(function(){
+        document.getElementById('copy-button').src = 'https://cdn-icons-png.flaticon.com/512/4288/4288159.png'
+    }, 100)
+        document.getElementById('copy-button').src = 'https://cdn-icons-png.flaticon.com/512/4280/4280618.png' 
+        navigator.clipboard.writeText(content.innerText)
 }
