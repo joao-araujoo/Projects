b6f32837dc00f01c9634c8d012ad2a60e5422044
joao-araujoo <joao.araujoo2007@gmail.com>
2023-01-12 10:49:20 -0300

    confirmation

--- To-do list/script.js
+++ To-do list/script.js
@@ -3,23 +3,28 @@ addButton.addEventListener('click', addTask)
 
 function addTask(){
     const taskText = document.querySelector('input#taskText').value
-    const tasksSection = document.querySelector('div.tasks')
-
-    const task = document.createElement('div')
-    task.className = 'task'
-
-    const taskParagraph = document.createElement('p')
-    taskParagraph.innerText = taskText
-
-    const doneButton = document.createElement('button')
-    doneButton.innerText = '✔️'
-
-    const removeButton = document.createElement('button')
-    removeButton.innerText = '❌'
-    removeButton.addEventListener('click', function(){
-        task.remove()
-    })
-
-    task.append(taskParagraph, doneButton, removeButton)
-    tasksSection.appendChild(task)
+    if(taskText !== ''){
+        const tasksSection = document.querySelector('div.tasks')
+    
+        const task = document.createElement('div')
+        task.className = 'task'
+    
+        const taskParagraph = document.createElement('p')
+        taskParagraph.innerText = taskText
+    
+        const doneButton = document.createElement('button')
+        doneButton.innerText = '✔️'
+    
+        const removeButton = document.createElement('button')
+        removeButton.innerText = '❌'
+        removeButton.addEventListener('click', function(){
+            task.remove()
+        })
+    
+        task.append(taskParagraph, doneButton, removeButton)
+        tasksSection.appendChild(task)
+        document.querySelector('input#taskText').value = ''
+    } else {
+        alert('Insira alguma tarefa!')
+    }
 }
