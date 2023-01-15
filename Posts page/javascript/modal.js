5272b435c21dd821cb9e65508ada36a0b5b80706
joao-araujoo <joao.araujoo2007@gmail.com>
2023-01-15 12:01:37 -0300

    deleting modal fix

--- Posts page/javascript/modal.js
+++ Posts page/javascript/modal.js
@@ -20,7 +20,7 @@
 
 function closeModal(){
     document.getElementById('modal').style.top = '-100%'
-    document.getElementById('modal-input-title').remove()
+    document.getElementById('modal-profile').remove()
     document.getElementById('modal-content-input').remove()
     document.getElementById('modal-post-button').remove()
 }
