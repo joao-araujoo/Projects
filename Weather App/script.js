6a84ee268bd195ca57b862149fee5cc8c203a899
joao-araujoo <joao.araujoo2007@gmail.com>
2023-08-30 20:31:02 -0300

    adding some style and basic js

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,34 +1,56 @@
 const apiKey = '1485f25e8514e046a28d8d5c74a9420a'
 
-// temperatura = main.temp
-// sensação = main.feels_like
-// temperatura mínima = main.temp_min
-// temperatura máxima = main.temp_max
-// umidade = main.humidity
+const daysOfWeek = [
+    "Sunday", "Monday", "Thuesday",
+    "Wednesday", "Thursday", "Friday", "Saturday"
+];
 
-// condições meteorológicas (chuva, neve, nuvens) = weather.main
-// descrição da condição meteorológica = weather.description
-// ícone da condição meteorológica = `https://openweathermap.org/img/wn/${weather.icon}.png`
+const months = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
 
-// código do país = sys.country
+function getDate(){
+    const now = new Date();
+    
+    const dayOfWeek = daysOfWeek[now.getDay()];
+    const day = now.getDate();
+    const month = months[now.getMonth()];
+
+    return {dayOfWeek, day, month}
+}
+
+function changeCard(){
+    document.querySelector('.search-card').classList.toggle('hidden')
+    document.querySelector('.forecast-card').classList.toggle('hidden')
+    setTimeout(() => {
+        document.querySelector('.forecast-card .header').classList.toggle('skeleton')
+        document.querySelector('.forecast-card .main-stats').classList.toggle('skeleton')
+        document.querySelector('.forecast-card .extra-stats').classList.toggle('skeleton')
+    }, 1000)
+}
 
 const getWeatherData = async (city) => {
     return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`).then(response => response.json())
 }
 
-async function getTemperature(city) {
+async function getWeather() {
+    const city = document.querySelector('#city-input').value
     const weatherData = await getWeatherData(city)
 
-    const divStats = document.querySelector('.stats')
-    divStats.innerHTML += `<b>Temperatura:</b> ${weatherData.main.temp}°C <br>`
-    divStats.innerHTML += `<b>Sensação:</b> ${weatherData.main.feels_like}°C <br>`
-    divStats.innerHTML += `<b>Temperatura mínima:</b> ${weatherData.main.temp_min}°C <br>`
-    divStats.innerHTML += `<b>Temperatura máxima:</b> ${weatherData.main.temp_max}°C <br>`
-    divStats.innerHTML += `<b>Umidade:</b> ${weatherData.main.humidity}% <br>`
-    divStats.innerHTML += '<br><b>Condições meteorológicas:</b> ' + weatherData.weather[0].main
-    divStats.innerHTML += '<br><b>Descrição:</b> ' + weatherData.weather[0].description
-    document.querySelector('#weather-icon').src = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`
-    document.querySelector('#country-flag').src = `https://flagsapi.com/${weatherData.sys.country}/flat/64.png`
-}
+    // temperature
+    document.querySelector('.temp h2').innerText = `${weatherData.main.temp.toFixed(0)}°`
 
-getTemperature('hong kong')
+    // date
+    const {dayOfWeek, day, month} = getDate()
+    document.querySelector('.date .day-of-week').innerText = dayOfWeek
+    document.querySelector('.date .day-month').innerText = `${day} ${month}`
+
+    // weather icon
+    document.querySelector('.weather-icon').src = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`
+
+    // location
+    document.querySelector('.location').innerText = `${weatherData.name}, ${weatherData.sys.country}`
+
+    changeCard()
+}
