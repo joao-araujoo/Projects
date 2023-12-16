8243213e5bbc4dfffb8106b7e658f8c23f35ff56
joao araujo <joao.araujoo2007@gmail.com>
2023-12-16 11:26:34 -0300

    feat: register page

---
+++ ÉlégantCart/src/pages/Register.jsx
@@ -0,0 +1,20 @@
+export default function Register() {
+  return (
+    <>
+      <main>
+        <div className="form-container">
+          <h1>Register</h1>
+          <form>
+            <label htmlFor="username">Username</label>
+            <input type="text" name="username" id="username" style={{marginBottom: "30px"}} />
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" style={{marginBottom: "30px"}} />
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" style={{marginBottom: "30px"}} />
+            <button type="submit" style={{ marginTop: "20px" }}>Register</button>
+          </form>
+        </div>
+      </main>
+    </>
+  )
+}
--- ÉlégantCart/src/router.jsx
+++ ÉlégantCart/src/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />
+  },
+  {
+    path: "/register",  
+    element: <Register />
   }
 ])
 
